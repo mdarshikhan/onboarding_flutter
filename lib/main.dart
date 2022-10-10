8b0649bfeaebf66350ecd585e13b149c1dc94cc3
dkamalsingh <deepak.kamalsingh@dxc.com>
2022-10-10 13:17:43 +0530

    [Deepak][JIRA-MPU-0000] : Removed unused code & optimized the code.

--- lib/main.dart
+++ lib/main.dart
@@ -49,18 +49,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -123,22 +111,10 @@ class OnBoardingPage extends StatefulWidget {
 }
 
 class _OnBoardingPageState extends State<OnBoardingPage> {
-  int _counter = 0;
   final controller = PageController();
   bool isLastPage = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-   //Build page Widget for each Slider
+  //Build page Widget for each Slider
   Widget buildPage({
   required Color color,
     required String urlImage,
